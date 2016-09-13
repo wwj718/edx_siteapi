c889315824fedeebcf4405a4efae4777f7506fd4
wwj718 <wuwenjie718@gmail.com>
2016-09-13 09:50:13 +0800

    Update create_user_from_csv.py

--- management/commands/create_user_from_csv.py
+++ management/commands/create_user_from_csv.py
@@ -9,7 +9,8 @@ from student.views import _do_create_account, AccountValidationError
 from track.management.tracked_command import TrackedCommand
 # 解析csv
 import unicodecsv  # utf-8 ,也可以用pandas:
-
+#from opaque_keys.edx.locations import SlashSeparatedCourseKey
+#from student.models import CourseEnrollment
 
 def create_user(username, password, email, name):
     form = AccountCreationForm(data={
@@ -59,5 +60,7 @@ class Command(TrackedCommand):
                 name = item[u"name"]
                 password = username
                 create_user(username, password, email, name)
+                #course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+                #CourseEnrollment.enroll(user, course_key)
         # 缺乏读写csv的技巧,next和边界，按header读取
         # http://python3-cookbook.readthedocs.io/zh_CN/latest/c06/p01_read_write_csv_data.html
